17f336fe02fa6df9b0d702227c07ba3589cb7c10
Rmando42 <aremcaar@outlook.com>
2025-11-16 17:44:14 -0600

    implemented Controller using caller and loteria card deck

--- LoteriaGame/src/GameFiles/Controller.java
+++ LoteriaGame/src/GameFiles/Controller.java
@@ -2,4 +2,32 @@ package GameFiles;
 
 public class Controller {
 
+    private final Caller caller;
+
+    public Controller(){
+        DeckOfCards<LoteriaCard> deck = new DeckOfCards<>(LoteriaCard.class);
+        this.caller = new Caller(deck);
+    }
+
+    // Start game and shuffle the deck
+    public void startGame(){
+        caller.reshuffle();
+        System.out.println("Game Started");
+    }
+
+    // Move to next card
+    public LoteriaCard nextCard(){
+        return caller.callNext();
+    }
+
+    // Check for winner
+    public void checkWin(){
+        System.out.println("Game Won");
+    }
+
+    public boolean handleLoteria(Player player){
+        System.out.println("handling loteria");
+        return false;
+    }
+
 }
